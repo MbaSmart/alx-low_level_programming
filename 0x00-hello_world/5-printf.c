b2962cdba530a2728b7bf24e9735e95ae1f18780
MbaSmart <mbasmart007@gmail.com>
2022-07-01 04:07:21 -0700

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -10,6 +10,6 @@
 
 int main(void)
 {
-printf("\"with proper grammar, but the outcome is a piece of art");
-return (0);
+	printf("\"with proper grammar, but the outcome is a piece of art");
+	return (0);
 }
