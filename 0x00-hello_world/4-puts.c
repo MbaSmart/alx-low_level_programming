01f841f3ed9a697740777877fd5120678ee705c8
MbaSmart <mbasmart007@gmail.com>
2022-06-30 18:10:56 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-/*
- *C Programming requires meticulous attention
+/**
+ * main - main block
+ * Return: 0
  */
 
 int main(void)
+
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
