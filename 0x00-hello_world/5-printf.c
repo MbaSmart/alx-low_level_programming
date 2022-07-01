5e68188279319e7eb820e78af3f0c98b79aa5fa1
MbaSmart <mbasmart007@gmail.com>
2022-06-30 18:27:39 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/*
+ * mani - main block
+ * A piece of art by ALX
+ * Return: 0
+ */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
-	return (0);
+printf("\"with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
