1adbd676a69d64ac7057186ed28694781417c627
MbaSmart <mbasmart007@gmail.com>
2022-07-05 16:57:30 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 -> Success
+*
+*Return: 0 (Success)
+*
+*/
+
+void print_alphabet_x10(void)
+{
+	char i;
+
+	i = 0;
+
+	while (i< 10)
+	{
+		char n;
+
+			for (n = 'a'; n <= 'z'; n++)
+			{
+				_putchar(n);
+			}
+	_putchar('\n');
+	i++;
+	}
+}
