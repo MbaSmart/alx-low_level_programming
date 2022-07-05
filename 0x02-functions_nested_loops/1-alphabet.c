92fc1d3a09957de350d035df653f363d5ef5b222
MbaSmart <mbasmart007@gmail.com>
2022-07-05 16:08:19 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_alphabet -> Write a function that prints the alphabet,
+*in lowercase, followed by a new line.
+*
+*Return: 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
