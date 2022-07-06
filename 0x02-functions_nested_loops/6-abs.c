cd17651489caf0e857e6ebccc013555068ba0f67
MbaSmart <mbasmart007@gmail.com>
2022-07-05 20:28:26 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _abs -> Write a function that computes the
+* absolute value of an integer.
+*
+* @n: This indicates an integer
+*
+* Return: 0 (Success)
+*/
+
+int _abs(int n)
+{
+	return (n >= 0 ? n : n * -1);
+}
