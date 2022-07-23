e66061c783f5f040863288af3f3629e6aaf4dab7
MbaSmart <mbasmart007@gmail.com>
2022-07-23 08:41:38 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,13 @@
 #include <stdio.h>
 
 /**
+ * main -  a program that prints its name,
+ * followed by a new line.
  *
+ * @argc: argument count.
+ * @argv: arguments.
  *
- *
- *
+ * Return: (0) Success.
  */
 
 int main(int argc, char *argv[])
