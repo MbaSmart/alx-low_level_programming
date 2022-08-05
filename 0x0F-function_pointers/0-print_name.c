e4525f7149ae1a346a57c498ae29dcadfc321928
MbaSmart <mbasmart007@gmail.com>
2022-08-05 09:18:06 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - this prints a name.
+ *
+ * @name: the name.
+ * @f: the function to print with.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
